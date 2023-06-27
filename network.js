58a0074f1831837941c2bee22758540b67d8c285
pandorabok <borkovicbiljana9@gmail.com>
2023-06-27 13:38:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -2,3 +2,4 @@
 // register() is not called by default.
 
 // This lets the app load faster on subsequent visits in production, and gives
+// it offline capabilities. However, it also means that developers (and users)
