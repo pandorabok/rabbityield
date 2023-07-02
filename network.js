797dfb3c3176845e35a95b86610a23b0c436ce14
pandorabok <borkovicbiljana9@gmail.com>
2023-07-02 11:36:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -7,3 +7,4 @@
 // existing tabs open on the page have been closed, since previously cached
 // resources are updated in the background.
 
+// To learn more about the benefits of this model and instructions on how to
