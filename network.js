b8dd0b1a68c432a9def86628c7cc9dbad388a9f8
pandorabok <borkovicbiljana9@gmail.com>
2023-08-26 15:51:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -62,3 +62,4 @@ function registerValidSW(swUrl, config) {
         }
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
