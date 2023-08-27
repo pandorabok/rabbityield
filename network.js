ff1f74861ddead2634c9f7c5e22927bf9c6e3318
pandorabok <borkovicbiljana9@gmail.com>
2023-08-27 09:38:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -63,3 +63,4 @@ function registerValidSW(swUrl, config) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
+              // At this point, the updated precached content has been fetched,
