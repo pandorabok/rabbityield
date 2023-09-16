d4f7f2e1925d70ae9a0b731c1957c3a564d924bf
pandorabok <borkovicbiljana9@gmail.com>
2023-09-16 14:20:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -83,3 +83,4 @@ function registerValidSW(swUrl, config) {
               // Execute callback
               if (config && config.onSuccess) {
                 config.onSuccess(registration);
+              }
