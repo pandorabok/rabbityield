3ed8d663d30476a60fc31f1574b016ce3f08c1b7
pandorabok <borkovicbiljana9@gmail.com>
2025-03-10 18:13:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -189,3 +189,4 @@ export function register(config) {
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
+// This lets the app load faster on subsequent visits in production, and gives
