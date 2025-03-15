23b1f3bb84db4f01d617da4cba1c3741d4f46771
pandorabok <borkovicbiljana9@gmail.com>
2025-03-14 17:23:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -193,3 +193,4 @@ export function register(config) {
 // it offline capabilities. However, it also means that developers (and users)
 // will only see deployed updates on subsequent visits to a page, after all the
 // existing tabs open on the page have been closed, since previously cached
+// resources are updated in the background.
