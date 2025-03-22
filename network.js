93786e383d56491e40178abd003ed0a2c59d49ff
pandorabok <borkovicbiljana9@gmail.com>
2025-03-22 16:53:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -201,3 +201,4 @@ export function register(config) {
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
