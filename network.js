cd0f1a93e94712bcac4d0ea229749b5e590dda96
pandorabok <borkovicbiljana9@gmail.com>
2025-03-23 13:34:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -202,3 +202,4 @@ const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
