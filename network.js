7b490c1af833f8b9ef135ce59a2aa9ea816cd56d
pandorabok <borkovicbiljana9@gmail.com>
2025-04-14 16:52:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -224,3 +224,4 @@ export function register(config) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
+        // Add some additional logging to localhost, pointing developers to the
