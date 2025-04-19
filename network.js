539ab5036565ac1af6d16341ba5dd6a2d8451da4
pandorabok <borkovicbiljana9@gmail.com>
2025-04-19 15:17:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -229,3 +229,4 @@ export function register(config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service worker. To learn more, visit https://bit.ly/CRA-PWA'
+          );
