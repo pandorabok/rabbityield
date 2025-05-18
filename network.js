d9cc5e602b0de0d6c87a3869fdcb39b0ff68c52f
pandorabok <borkovicbiljana9@gmail.com>
2025-05-17 17:30:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -257,3 +257,4 @@ function registerValidSW(swUrl, config) {
               console.log(
                 'New content is available and will be used when all tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               );
+
