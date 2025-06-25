f38f2cb3fd4510d85cd347e05adfa448c71609a4
pandorabok <borkovicbiljana9@gmail.com>
2025-06-25 16:56:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -296,3 +296,4 @@ function checkValidServiceWorker(swUrl, config) {
       ) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
+          registration.unregister().then(() => {
