ef8bf68d3e7e44d6848c557652e380f118d42847
pandorabok <borkovicbiljana9@gmail.com>
2025-07-24 13:41:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -325,3 +325,4 @@ export function unregister() {
 // register() is not called by default.
 
 // This lets the app load faster on subsequent visits in production, and gives
+// it offline capabilities. However, it also means that developers (and users)
