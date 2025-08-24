4ed4e5c899a648f543fd6977eb2f056ade6aced4
pandorabok <borkovicbiljana9@gmail.com>
2025-08-24 14:06:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -356,3 +356,4 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
+        // This is running on localhost. Let's check if a service worker still exists or not.
