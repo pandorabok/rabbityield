f624f12a7457c2e7387824977120833cce8f96fa
pandorabok <borkovicbiljana9@gmail.com>
2025-08-29 14:22:00 -0700

    Development progress - updating contract functionality

--- network.js
+++ network.js
@@ -361,3 +361,4 @@ export function register(config) {
 
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
+        navigator.serviceWorker.ready.then(() => {
